8f4f28d5c49b4e2e6f79ac32e164be57ec4d413b
bernardofcs <bernardofcs@hotmail.com>
2017-03-14 19:32:01 +0000

    completed exercise 9

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -6,6 +6,7 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 require_relative './exercise_7'
+
 @store1 = Store.first
 output = @store1.employees.create(first_name: 'Robert', last_name: 'California', hourly_rate: 180)
 puts output.password
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
-  belongs_to :store, required: true     
+  belongs_to :store, required: true    
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, inclusion: { in: 40..200 }
